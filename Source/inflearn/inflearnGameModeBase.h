392201becc9867e505c4d9567fc8e8b0b33e8590
sarah2234 <2234sarah@gmail.com>
2022-08-25 21:02:46 +0900

    inflearn-언리얼 엔진4  입문 섹션 0 환경 설정

---
+++ Source/inflearn/inflearnGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "inflearnGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INFLEARN_API AinflearnGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
